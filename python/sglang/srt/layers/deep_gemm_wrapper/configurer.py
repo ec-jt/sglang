ffcd2b7ac16d8ea32dcce1bf70634ffb15662ec5
ec-jt <james.trappett@elementalcompute.com>
2026-02-19 11:51:08 +0000

    nsa

--- python/sglang/srt/layers/deep_gemm_wrapper/configurer.py
+++ python/sglang/srt/layers/deep_gemm_wrapper/configurer.py
@@ -1,7 +1,7 @@
 import logging
 
 from sglang.srt.environ import envs
-from sglang.srt.utils import get_device_sm, is_blackwell_supported
+from sglang.srt.utils import get_device_sm, is_blackwell_supported, is_sm120_supported
 
 logger = logging.getLogger(__name__)
 
@@ -22,4 +22,9 @@ def _compute_enable_deep_gemm():
 ENABLE_JIT_DEEPGEMM = _compute_enable_deep_gemm()
 
 DEEPGEMM_BLACKWELL = ENABLE_JIT_DEEPGEMM and is_blackwell_supported()
-DEEPGEMM_SCALE_UE8M0 = DEEPGEMM_BLACKWELL
+# SM120 (RTX 5090) does not support UE8M0 scale format (TCGEN05 feature)
+# Only SM100/SM101 (data center Blackwell) supports it
+DEEPGEMM_SCALE_UE8M0 = DEEPGEMM_BLACKWELL and not is_sm120_supported()
+
+if ENABLE_JIT_DEEPGEMM and is_sm120_supported():
+    logger.info("SM120 detected: DeepGEMM using SM89 MMA atom (mma.sync.m16n8k32) for FP8 kernels")
