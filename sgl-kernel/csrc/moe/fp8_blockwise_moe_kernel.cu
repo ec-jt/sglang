d74f9796c32f26744c8a62d5223984b58ddb8035
ec-jt <james.trappett@elementalcompute.com>
2026-02-22 13:17:52 +0000

    fp8 sm120 moe

--- sgl-kernel/csrc/moe/fp8_blockwise_moe_kernel.cu
+++ sgl-kernel/csrc/moe/fp8_blockwise_moe_kernel.cu
@@ -712,6 +712,7 @@ void fp8_blockwise_scaled_grouped_mm(
 #if CUDA_VERSION >= 12090
       || sm_version == 103
 #endif
+      || sm_version == 120
   ) {
     if (output.scalar_type() == torch::kBFloat16) {
       sm100_fp8_blockwise_group_mm_dispatch_shape<cutlass::bfloat16_t>(
